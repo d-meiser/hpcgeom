ebb21675be27b7a997026d754f0c48b6b2dc8058
Dominic Meiser <dmeiser79@gmail.com>
2018-07-17 22:50:25 -0600

    Add a hash table data structure

---
+++ include/hash_table.h
@@ -0,0 +1,29 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <geo_export.h>
+#include <stdint.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct GeoHashTable
+{
+  void *impl;
+};
+
+GEO_EXPORT void GeoHTInitialize(struct GeoHashTable *ht);
+GEO_EXPORT void GeoHTDestroy(struct GeoHashTable *ht);
+GEO_EXPORT void GeoHTInsert(struct GeoHashTable *ht, uint32_t key,
+	uint32_t value);
+GEO_EXPORT int GeoHTLookup(struct GeoHashTable *ht, uint32_t key,
+	uint32_t *value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ src/hash_table.cpp
@@ -0,0 +1,33 @@
+#include <hash_table.h>
+
+#include <unordered_map>
+
+struct GeoHashTableImpl {
+  std::unordered_map<uint32_t, uint32_t> map;
+};
+
+void GeoHTInitialize(struct GeoHashTable *ht) {
+  ht->impl = new GeoHashTableImpl;
+}
+
+void GeoHTDestroy(struct GeoHashTable *ht) {
+  delete (GeoHashTableImpl*)ht->impl;
+}
+
+void GeoHTInsert(struct GeoHashTable *ht, uint32_t key, uint32_t value) {
+  struct GeoHashTableImpl *impl =
+      static_cast<struct GeoHashTableImpl *>(ht->impl);
+  impl->map[key] = value;
+}
+
+int GeoHTLookup(struct GeoHashTable *ht, uint32_t key, uint32_t *value) {
+  struct GeoHashTableImpl *impl =
+      static_cast<struct GeoHashTableImpl *>(ht->impl);
+  auto i = impl->map.find(key);
+  if (i != impl->map.end()) {
+    *value = i->second;
+    return 1;
+  }
+  return 0;
+}
+
