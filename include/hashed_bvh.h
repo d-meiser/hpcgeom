e813ffc1927d04ace1d8ea76b18dc94fbc3319c4
Dominic Meiser <dmeiser79@gmail.com>
2018-07-28 13:06:28 -0600

    Add insert and visit methods to bvh class

--- include/hashed_bvh.h
+++ include/hashed_bvh.h
@@ -22,6 +22,14 @@ struct GeoHashedBvh {
 GEO_EXPORT void GeoHBInitialize(struct GeoHashedBvh *bvh,
 	struct GeoBoundingBox bbox);
 GEO_EXPORT void GeoHBDestroy(struct GeoHashedBvh *bvh);
+GEO_EXPORT void GeoHBInsert(struct GeoHashedBvh *bvh, int n,
+	const struct GeoBoundingBox *volumes, const void *data);
+typedef int GeoVolumeVisitor(struct GeoBoundingBox *volumes, void *data, int i,
+	void *ctx);
+GEO_EXPORT void GeoHBVisitIntersectingVolumes(struct GeoHashedBvh *bvh,
+	const struct GeoBoundingBox *volume,
+	GeoVolumeVisitor visitor,
+	void *ctx);
 
 
 #ifdef __cplusplus
